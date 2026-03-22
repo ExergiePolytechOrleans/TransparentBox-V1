def4197c0b8602313d6a552e6a86bfadb4885d68
Hector van der Aa <hector@h3cx.dev>
2026-03-22 22:53:51 +0100

    Started main.cpp init

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
-#include <Arduino.h>
+#include "flags.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduino.h>
+#include "modules/lcd/lcd.h"
+#include "modules/gps/gps.h"
+#include "modules/logger/system_logger.h"
+
+
+system_logger *logger_output = new system_logger(&Serial);
+
+lcd *driver_display = new lcd();
+gps *gps_module = new gps(&Serial2, logger_output);
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  driver_display->init();
+  driver_display->print_message("Starting Initialization");
+  delay(1000);
+
+  driver_display->print_message("Serial Init...");
+  Serial.begin(115200);
+  delay(500);
+  driver_display->print_message("Serial Init Complete");
+  delay(500);
+  
+  driver_display->print_message("GPS Init...");
+  gps_module->init();
+  delay(500);
+  driver_display->print_message("GPS Init Complete");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  gps_module->parse_task(500);
 }
