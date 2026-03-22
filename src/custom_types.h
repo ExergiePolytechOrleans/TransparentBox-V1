b8fc76af69298646793ca799a7b4dd7d7d99e35c
Hector van der Aa <hector@h3cx.dev>
2026-03-22 23:16:36 +0100

    Updated custom_types.h with new magic header

--- src/custom_types.h
+++ src/custom_types.h
@@ -1,9 +1,11 @@
 #include <inttypes.h>
+
+#define CONFIG_MAGIC 0xBEEF
 typedef struct vehicle_config{
-    uint16_t config_lock;
-    bool auto_detect_track;
-    uint8_t num_tracks;
-    uint8_t selected_track;
+    uint16_t magic = CONFIG_MAGIC;
+    bool auto_detect_track = true;
+    uint8_t num_tracks = 0;
+    uint8_t selected_track = 0;
 };
 
 typedef struct track_data {
