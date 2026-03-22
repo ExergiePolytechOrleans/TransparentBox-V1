9df359e63e6a5c3197efd0ba92fec76ae1ce7e29
Hector van der Aa <hector@h3cx.dev>
2026-03-22 23:51:01 +0100

    Update custom_types.h to simple struct

--- src/custom_types.h
+++ src/custom_types.h
@@ -1,14 +1,14 @@
 #include <inttypes.h>
 
 #define CONFIG_MAGIC 0xBEEF
-typedef struct vehicle_config{
+struct vehicle_config{
     uint16_t magic = CONFIG_MAGIC;
     bool auto_detect_track = true;
     uint8_t num_tracks = 0;
     uint8_t selected_track = 0;
 };
 
-typedef struct track_data {
+struct track_data {
     char name[64];
     
 };
