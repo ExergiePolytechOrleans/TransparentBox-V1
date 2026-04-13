e46f0e92c27e2f0284fcbde64d0aaac8160cfa3a
Hector van der Aa <hector@h3cx.dev>
2026-04-13 20:18:21 +0200

    Update telemetry to standardize cs timestamps

--- src/modules/telemetry/telemetry.cpp
+++ src/modules/telemetry/telemetry.cpp
@@ -31,7 +31,7 @@ int Telemetry::loop() {
         vbatGlobalRead(packet.vbat);
         GpsData gps;
         gpsGlobalRead(gps);
-        packet.time_stamp = gps.time_;
+        packet.time_stamp = hhmmsscc_to_cs(gps.time_);
         packet.lat = gps.lat_.value_;
         packet.lng = gps.lng_.value_;
         packet.speed = gps.speed_.value_;
